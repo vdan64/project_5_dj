55b224c1d8e3c7e5631ce9c9287f7b9e4628f683
victors <victordaniel64x@gmail.com>
2024-02-14 11:10:35 -0400

    added mejor

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import post
 
 # Create your views here.
+class BlogListView(ListView):
+    model = post
+    template_name = "home.html"
