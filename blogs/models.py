e13162354e73bc73c5600a330e597098d0fe3460
victors <victordaniel64x@gmail.com>
2024-02-21 10:17:01 -0400

    delete update

--- blogs/models.py
+++ blogs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
-class post(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     author = models.ForeignKey(
